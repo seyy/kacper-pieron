3b9a571607884ad30d9112ed5829063f40bf9eb4
kacper.pieron <kpieron@poczta.fm>
2023-08-28 19:09:52 +0200

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,64 @@
 'use client'
 
-import { Navigation } from "../components/Navigation"
-import styled from 'styled-components'
-import AboutMe from "../pages/AboutMe"
-import Work from "../pages/Work"
-import Contact from "../pages/Contact"
-import { useEffect, useState } from "react"
+import { Navigation } from "../components/Navigation";
+import styled from 'styled-components';
+import AboutMe from "../pages/AboutMe";
+import Work from "../pages/Work";
+import Contact from "../pages/Contact";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
-      setIsLoading(false)
-    }, 900)
-  }, [])
+      setIsLoading(false);
+    });
+  }, []);
 
   return (
-    <>
-    <NavContainer>
-      <Navigation />
-    </NavContainer>
-      <section id="about">
-        <AboutMe />
-      </section>
-      <section id="work">
-        <Work />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
-    </>
-  )
+    <Container>
+        {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <NavContainer>
+            <Navigation />
+          </NavContainer>
+          <ContentContainer>
+            <section id="about">
+              <AboutMe />
+            </section>
+            <section id="work">
+              <Work />
+            </section>
+            <section id="contact">
+              <Contact />
+            </section>
+          </ContentContainer>
+        </>
+      )}
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  position: relative;
+`;
 
 const NavContainer = styled.div`
   position: fixed;
-  top: 0px;
+  top: 0;
   width: 100%;
-  background-color: white;
-`;
+  z-index: 10000;
+`
+
+const Loader = styled.div`
+  background-color: black;
+`
+
+const ContentContainer = styled.div`
+  position: relative;
+  z-index: 1;
+`
+
